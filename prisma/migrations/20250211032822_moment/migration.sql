a292ee4e1651fd00817e8eec432c1a7a5175f269
hprasath2306 <hariprasaths21cs@psnacet.edu.in>
2025-02-12 16:54:56 +0530

    Deploy

---
+++ prisma/migrations/20250211032822_moment/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Moment" (
+    "id" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Moment_pkey" PRIMARY KEY ("id")
+);
---
+++ src/index.ts
@@ -0,0 +1,96 @@
+import express, { urlencoded } from "express";
+import prisma from "./db";
+import cors from "cors";
+import serverless from "serverless-http";
+
+
+const app = express();
+app.use(urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.send("Server is ready!!!");
+});
+
+app.get("/api/users", async (req, res) => {
+    const users = await prisma.user.findMany();
+    res.json(users);
+});
+
+app.get("/api/users/:event", async (req, res) => {
+    const eventName = req.params.event;
+    const users = await prisma.user.findMany({
+        where: {
+            event: eventName,
+        },
+    });
+    if (users.length === 0) {
+        res.status(404).json({ message: "No users found for this event" });
+    } else {
+        res.json(users);
+    }
+});
+
+app.post("/api/users", async (req, res) => {
+    try {
+        if (!req.body.name || !req.body.email || !req.body.year || !req.body.section || !req.body.event) {
+            res.status(400).json({ message: "Please fill all fields" });
+            return;
+        }
+        const { name, email, year, section, event, phone } = req.body;
+        // console.log(name, email, year, section, event);
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                year,
+                section,
+                event,
+                phone,
+            },
+        });
+        res.json(user);
+    } catch (e: any) {
+        res.status(400).json({ message: e.message });
+        return;
+    }
+});
+
+app.post("/api/moments", async (req, res) => {
+    try{
+        const imageUrl = req.body.imageUrl;
+        console.log(imageUrl)
+        const moment = await prisma.moment.create({
+            data: {
+                imageUrl,
+            },
+        });
+        res.json(moment);
+    }catch(e: any){
+        res.status(400).json({ message: e.message });
+        return;
+    }
+})
+
+app.get('/api/moments', async (req, res) => {
+    try {
+      const moments = await prisma.moment.findMany({
+        orderBy: {
+          createdAt: 'desc'
+        }
+      });
+      res.json(moments);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to fetch moments' });
+    }
+  });
+
+const port = process.env.PORT || 5000;
+if (process.env.NODE_ENV === "dev") {
+    app.listen(port, () => {
+        console.log(`Server is running`);
+    });
+}
+
+export const handler = serverless(app);
