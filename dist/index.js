fe7f7f04ca5728e082832f9bd27cb34263eee595
hprasath2306 <hariprasaths21cs@psnacet.edu.in>
2025-02-23 18:42:45 +0530

    Heetah

---
+++ dist/index.js
@@ -0,0 +1,25 @@
+import express, { urlencoded } from "express";
+import prisma from "./db/index.js";
+import cors from "cors";
+import studentRoutes from './routes/students.js';
+import momentRoutes from './routes/moment.js';
+const app = express();
+app.use(urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => {
+    res.send("Server is ready!!!");
+});
+app.get("/api/users", async (req, res) => {
+    const users = await prisma.student.findMany();
+    res.json(users);
+});
+app.use('/api/students', studentRoutes);
+app.use('/api', momentRoutes);
+const port = process.env.PORT || 5000;
+// if (process.env.NODE_ENV === "dev") {
+app.listen(port, () => {
+    console.log(`Server is running`);
+});
+// }
+// export const handler = serverless(app);
---
+++ dist/routes/moment.js
@@ -0,0 +1,34 @@
+import { Router } from 'express';
+import { PrismaClient } from '@prisma/client';
+const router = Router();
+const prisma = new PrismaClient();
+router.post("/moments", async (req, res) => {
+    try {
+        const imageUrl = req.body.imageUrl;
+        console.log(imageUrl);
+        const moment = await prisma.moment.create({
+            data: {
+                imageUrl,
+            },
+        });
+        res.json(moment);
+    }
+    catch (e) {
+        res.status(400).json({ message: e.message });
+        return;
+    }
+});
+router.get('/moments', async (req, res) => {
+    try {
+        const moments = await prisma.moment.findMany({
+            orderBy: {
+                createdAt: 'desc'
+            }
+        });
+        res.json(moments);
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Failed to fetch moments' });
+    }
+});
+export default router;
